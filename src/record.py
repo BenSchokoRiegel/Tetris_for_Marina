3d528202134c5c22a4be9901f28b8110caa261b9
ben <ben.riegel@mni.thm.de>
2021-02-01 21:07:57 +0100

    starten mit Rekorts

--- src/record.py
+++ src/record.py
@@ -1,29 +1,55 @@
+import pygame
+
+from src.main import top_left_y,top_left_x,play_width
 
 
-# What if there aren't 5 players
-def read_in_top_five(score):
+def draw_top_list(surface,names,score,place):
+    pygame.font.init()
+    heading = pygame.font.SysFont('comicsans', 60)
+    if place == 0:
+       label = heading.render("No Highscore. жалость")
+    else:
+        label = heading.render("You made it to place " + str(place) + "Хорошо!")
+
+    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 0))
+    pygame.display.update()
+
+
+def read_in_top_five(surface,pts):
     names = []
-    scores = []
-    place = 6
-    f = open("Records.txt", "r")
+    score = []
+    place = 0
+    f = open("Records/level1.txt", "r")
     for x in f:
-        line = x.split("\t")
-        names[0] = line[0]
-        scores[0] = line[1]
+        #print(x)
+        line = x.split()
+        names.append(line[0])
+        score.append(int(line[1]))
 
     for i in range(5):
         if len(score) <= i:
             print("You made it in the Top 5. поздравления!\n")
-            print("Your place is " + str(i) + "\n")
-            place = i
+            print("Your place is " + str(i+1) + "\n")
+            place = i +1
+            break
 
-        if i> score[i]:
+        if pts > score[i]:
             print("You made it in the Top 5. поздравления!\n")
-            print("Your place is " + str(i) + "\n")
-            place = i
+            print("Your place is " + str(i+1) + "\n")
+            place = i+1
+            break
 
-    if place > 5:
+    if place == 0:
         print("You did not made it in the Top 5.")
+        draw_top_list(surface, names, score, place)
+    else:
+        names.insert(place-1,"new")
+        score.insert(place-1,pts)
+        #nur liste bis 5 maximal oder weniger übergeben
+        for i in range(len(names),5):
+            names.append("-")
+            score.append(0)
+        draw_top_list(surface,names,score,place)
 
     f.close()
-    return [names,scores]
+   # return [names,scores]
