55faf7b46b877863841d7d736253a973cf4af53c
ben <ben.riegel@mni.thm.de>
2021-03-01 19:19:10 +0100

    create the top_files_5 picture

--- src/record.py
+++ src/record.py
@@ -1,21 +1,79 @@
+import textwrap
+
 import pygame
 
-from src.main import top_left_y,top_left_x,play_width
 
+def draw_top_list(surface,names,score,place, list_of_window_stuff):
+    top_left_x = list_of_window_stuff[0]
+    top_left_y = list_of_window_stuff[1]
+    play_width = list_of_window_stuff[2]
+    play_hight = list_of_window_stuff[3]
 
-def draw_top_list(surface,names,score,place):
     pygame.font.init()
+    surface.fill((255,255,255))
+    bg = pygame.image.load("pictures/Endkart Bilder/kissing.jpg")
+    surface.blit(bg,(0,0))
     heading = pygame.font.SysFont('comicsans', 60)
     if place == 0:
-       label = heading.render("No Highscore. жалость")
-    else:
-        label = heading.render("You made it to place " + str(place) + "Хорошо!")
+       label = heading.render("No Highscore. жалость",1, (255, 255, 255))
+       surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), play_hight - (play_hight / 1.2)))
+       for i in range(1, 6):
+           name = names[i - 1];
+           for _ in range((len(name)), 15):
+               name = name + " "
 
-    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 0))
+           text = str(i) + " : " + name + " = " + str(score[i - 1])
+           new_label = heading.render(text, 1, (255, 0, 0))
+           surface.blit(new_label, (
+           top_left_x + play_width / 2 - (label.get_width() / 2), play_hight - (play_hight / (1.8 + (0.1 * i * i)))))
+       pygame.display.update()
+       return
+
+    pl_name = ''
+    done = False
+    while not done:
+        surface.fill((255, 255, 255))
+        surface.blit(bg, (0, 0))
+        label = heading.render("You made it to place " + str(place) + ". Хорошо!",1, (0, 0, 0))
+        surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), play_hight - (play_hight / 1.2)))
+        label2 = heading.render("Enter your name :" + str(pl_name),1, (0, 0, 0))
+        surface.blit(label2, (top_left_x + play_width / 2 - (label.get_width() / 2), play_hight - (play_hight / 1.4)))
+        names[place - 1 ] = pl_name
+
+        label_highscore = heading.render("HighscoreList :" ,1, (255, 0, 0))
+        surface.blit(label_highscore, (top_left_x + play_width / 2 - (label.get_width() / 2), play_hight - (play_hight / 1.6)))
+
+
+        for i in range(1,6):
+            name = names[i - 1];
+            for _ in range((len(name)),15):
+                name = name + " "
+
+            text = str(i) + " : " + name + " = " + str(score[i-1])
+            new_label = heading.render(text,1,(255, 0, 0))
+            surface.blit(new_label,(top_left_x + play_width / 2 - (label.get_width() / 2), play_hight - (play_hight / (1.8 + (0.1 *i * i )))))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                break;
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+                    print("enter")
+                    done = True
+                elif event.key == pygame.K_BACKSPACE:
+                    pl_name = pl_name[0:len(pl_name)-1]
+                else:
+                    pl_name = pl_name + event.unicode
     pygame.display.update()
 
+def create_highscore_cheat():
+    pass
 
-def read_in_top_five(surface,pts):
+
+def read_in_top_five(surface, pts,list_of_window_stuff):
     names = []
     score = []
     place = 0
@@ -49,7 +107,7 @@ def read_in_top_five(surface,pts):
         for i in range(len(names),5):
             names.append("-")
             score.append(0)
-        draw_top_list(surface,names,score,place)
+        draw_top_list(surface,names,score,place,list_of_window_stuff)
 
     f.close()
    # return [names,scores]
